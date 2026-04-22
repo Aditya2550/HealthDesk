8d6ca6711b55662799b9b24a8839ee53444c137e
Aditya Valsangkar <adidev2550@gmail.com>
2026-04-22 20:39:57 +0530

    Appointment slots schema

---
+++ server/models/Slot.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const slotSchema = new mongoose.Schema(
+  {
+    doctorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    isBooked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Slot", slotSchema);
