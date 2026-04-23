e067affd5346430968b1642fd1cfb28aeee8f5a3
Aditya Valsangkar <adidev2550@gmail.com>
2026-04-23 11:57:20 +0530

    Slot updation function for Doctor route updation

--- server/routes/slotRoutes.js
+++ server/routes/slotRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addSlot, getSlots, deleteSlot } = require('../controllers/slotController');
+const { addSlot, getSlots, updateSlot, deleteSlot } = require('../controllers/slotController');
 const { protect, authorizeRoles } = require('../middleware/authMiddleware');
 
 router.get('/', getSlots); // Public
 router.post('/', protect, authorizeRoles('doctor', 'admin'), addSlot);
+router.put('/:id', protect, authorizeRoles('doctor', 'admin'), updateSlot);
 router.delete('/:id', protect, authorizeRoles('doctor', 'admin'), deleteSlot);
 
 module.exports = router;
